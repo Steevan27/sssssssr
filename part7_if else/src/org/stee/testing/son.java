3ed952ce86eb6fd999ac7575bd4f56aa664c2e09
Steevan27 <steevanmahen@gmail.com>
2022-11-30 21:09:06 +0530

    gdstjyfku

---
+++ part7_if else/src/org/stee/testing/son.java
@@ -0,0 +1,41 @@
+package org.stee.testing;
+
+public class son {
+	
+	
+	public static void main(String[] args) {
+		
+		//IF,ELSE STATMENT
+		
+		int i=10;
+		
+		if (i>=12) {
+			
+			
+			System.out.println("condition pass");
+			
+		} else {
+
+			System.out.println("condition faild");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
